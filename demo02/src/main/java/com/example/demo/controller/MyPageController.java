b11d7ae872b012b8311a85eb015cd4d879dc6060
IamPoi <ckddbs803@gmail.com>
2023-05-28 21:35:34 +0900

    2023-05-28 메일

--- demo02/src/main/java/com/example/demo/controller/MyPageController.java
+++ demo02/src/main/java/com/example/demo/controller/MyPageController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.MemberDTO;
+import com.example.demo.service.SendMail;
 
 import jakarta.servlet.http.HttpSession;
 
---
+++ demo02/src/main/java/com/example/demo/controller/VerificationMailController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class VerificationMailController {
+
+	@Autowired
+	private EmailService emailService;
+
+//
+	@Autowired
+	public VerificationMailController(EmailService emailService) {
+		this.emailService = emailService;
+	}
+
+	@GetMapping("verification")
+	public Map verification(@RequestParam Map<String, Object> map) {
+
+		Map<String, Object> result = new HashMap<>();
+
+		Random random = new Random();
+
+		String to = (String) map.get("email");
+		String numbers = String.valueOf((random.nextInt(900000) + 100000));
+
+		emailService.sendEmail(to, "OurMarket275 인증 메일", numbers);
+
+		result.put("numbers", numbers);
+
+		return result;
+	}
+
+}
